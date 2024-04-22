960f2953cf92d2ae10418831013e8276ebc6a04e
astronmax <astronmax@yandex.com>
2024-04-22 23:32:53 +0300

    compilation bugfix

--- include/peer.hpp
+++ include/peer.hpp
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <common.hpp>
+
+#include <QtNetwork/QUdpSocket>
+
+#include <queue>
+
+struct PeerInfo {
+    std::string nickname;
+    HostAddress address;
+    NatType nat_type;
+};
